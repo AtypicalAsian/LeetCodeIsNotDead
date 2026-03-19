4c2d984574bbef6cf14e5158da377f2b396039fd
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2026-03-18 20:46:52 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (88.65%) - LeetSync

---
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        tracker = [0] * 101
+        for person in logs:
+            tracker[person[0]-1950] += 1
+            tracker[person[1]-1950] -= 1
+        
+        prefixSumArr = [0] * 101
+        currSum = 0
+        maxPop = 0
+        earliest = 0
+        for i in range(101):
+            currSum += tracker[i]
+            if currSum > maxPop:
+                maxPop = currSum
+                earliest = i + 1950
+        return earliest
