e64a0bb96694229594dad13439139990910a2b70
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2026-04-06 21:11:27 -0700

    Time: 109 ms (33.33%) | Memory: 9.9 MB (83.33%) - LeetSync

---
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.go
@@ -0,0 +1,30 @@
+import "fmt"
+
+func getSumAbsoluteDifferences(nums []int) []int {
+    n := len(nums)
+    prefixLeft := make([]int, n)
+    prefixRight := make([]int, n)
+    res := make([]int, n)
+
+    currSum := 0
+    // Create left prefix sum array
+    for i:=0;i<len(nums);i++{
+        prefixLeft[i] = currSum
+        currSum += nums[i]
+    }
+
+    currSum = 0
+    // Create right prefix sum array
+    for i:=n-1; i>=0; i-- {
+        prefixRight[i] = currSum
+        currSum += nums[i]
+    }
+
+    val_at_i := 0
+    for i:=0; i < len(nums); i++ {
+        val_at_i = (nums[i] * i - prefixLeft[i]) + (prefixRight[i] - nums[i] * (len(nums)-i-1))
+        res[i] = val_at_i
+    }
+
+    return res
+}
