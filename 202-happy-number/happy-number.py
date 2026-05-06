3e89de0fce635d845d49af69406b59e971b74071
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2026-05-05 23:07:55 -0500

    Time: 2 ms (44.77%) | Memory: 19.2 MB (59.39%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,15 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen_numbers = set()
-        while n != 1 and n not in seen_numbers:
-            seen_numbers.add(n)
-            n = self.digit_sum(n)
-        return n==1
-        
+        seen_nums = set()
+        while (n != 1) and (n not in seen_nums):
+            seen_nums.add(n)
+            n = self.getSquaredDigitSum(n)
+        return n == 1
     
-    def digit_sum(self,n):
+    def getSquaredDigitSum(self,n):
         currSum = 0
         while n > 0:
-            currSum += (n%10)**2
+            digit = n % 10
+            currSum += digit ** 2
             n //= 10
         return currSum
