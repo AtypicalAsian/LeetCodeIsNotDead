12fe08b2f08056c933c462ff0a8b52001f1504fd
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2026-05-05 22:57:59 -0500

    Time: 133 ms (40.43%) | Memory: 68.5 MB (34.75%) - LeetSync

--- 1722-throne-inheritance/throne-inheritance.py
+++ 1722-throne-inheritance/throne-inheritance.py
@@ -2,8 +2,8 @@ class ThroneInheritance:
 
     def __init__(self, kingName: str):
         self.dead = set()
-        self.king = kingName
         self.family_tree = defaultdict(list)
+        self.king = kingName
 
     def birth(self, parentName: str, childName: str) -> None:
         self.family_tree[parentName].append(childName)
@@ -12,16 +12,16 @@ class ThroneInheritance:
         self.dead.add(name)
 
     def getInheritanceOrder(self) -> List[str]:
-        # DFS on family tree to get inheritance order
-        def dfs(person):
-            if person not in self.dead:
-                inh_order.append(person)
-            for child in self.family_tree[person]:
-                dfs(child)
-        inh_order = []
-        dfs(self.king)
-        return inh_order
-        
+        #DFS Pre-Order to get inheritance order
+        inheritance_order = []
+        def dfs(node,res):
+            if node not in self.dead:
+                res.append(node)
+            for child in self.family_tree[node]:
+                dfs(child,res)
+        dfs(self.king, inheritance_order)
+        return inheritance_order
+
 
 
 # Your ThroneInheritance object will be instantiated and called as such:
