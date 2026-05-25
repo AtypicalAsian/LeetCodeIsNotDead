d45dae4f6d1e73a99815c13d13cb01bc05f1e37a
Nicolas <68703714+AtypicalAsian@users.noreply.github.com>
2026-05-25 13:06:40 -0700

    Time: 45 ms (86.10%) | Memory: 19.2 MB (91.40%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -4,9 +4,8 @@ class Solution:
         if (rows + cols) != len(s3):
             return False
         dp = [[False]*(cols+1) for _ in range(rows+1)]
-        # Base Case
         dp[0][0] = True
-        
+
         # Fill first row
         for i in range(1,rows+1):
             dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
@@ -15,9 +14,9 @@ class Solution:
         for j in range(1,cols+1):
             dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
         
-        # Fill rest of table
+        # Fill dp table
         for i in range(1,rows+1):
             for j in range(1,cols+1):
                 dp[i][j] = (s1[i-1] == s3[i+j-1] and dp[i-1][j]) or (s2[j-1] == s3[i+j-1] and dp[i][j-1])
-
+        
         return dp[rows][cols]
